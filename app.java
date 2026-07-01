5cb42e12eace013ef4c57baaf61daa1e02220f9c
abdmouleh marwen <abdmouleh.marwen@gmail.com>
2026-07-01 23:29:20 +0100

    fix: résolution du conflit en combinant auth et stats

--- app.java
+++ app.java
@@ -2,6 +2,7 @@ package com.monprojet;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Système d'Authentification actif");
         System.out.println("Affichage des Statistiques");
     }
 }
