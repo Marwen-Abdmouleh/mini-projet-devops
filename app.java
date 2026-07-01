215938a0a74002ecd3e8379dde1404576610138e
abdmouleh marwen <abdmouleh.marwen@gmail.com>
2026-07-01 23:26:54 +0100

    feat: ajout du module statistiques

--- app.java
+++ app.java
@@ -2,6 +2,6 @@ package com.monprojet;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Système d'Authentification actif");
+        System.out.println("Affichage des Statistiques");
     }
 }
